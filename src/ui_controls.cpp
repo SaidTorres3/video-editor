ecf2601aa7366936535da74668248fbeac494045
SaidTorres3 <sesaid17@hotmail.com>
2025-08-27 16:20:19 -0600

    Changed the default convertion option

--- src/ui_controls.cpp
+++ src/ui_controls.cpp
@@ -266,22 +266,22 @@ void CreateControls(HWND hwnd)
    SendMessage(g_hCheckboxMergeAudio, BM_SETCHECK, BST_UNCHECKED, 0);
    ApplyDarkTheme(g_hCheckboxMergeAudio);
 
-    g_hRadioCopyCodec = CreateWindow(
-        L"BUTTON", L"Copy Codec",
-        WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON | WS_GROUP,
-        340, 515, 100, 20,
-        hwnd, (HMENU)ID_RADIO_COPY_CODEC,
-        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr);
-    ApplyDarkTheme(g_hRadioCopyCodec);
-    SendMessage(g_hRadioCopyCodec, BM_SETCHECK, BST_CHECKED, 0);
-
     g_hRadioH264 = CreateWindow(
         L"BUTTON", L"Convert to H264",
-        WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
-        445, 515, 160, 20, // ← Aumenta esto
+        WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON | WS_GROUP,
+        340, 515, 100, 20,
         hwnd, (HMENU)ID_RADIO_H264,
         (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr);
     ApplyDarkTheme(g_hRadioH264);
+    SendMessage(g_hRadioH264, BM_SETCHECK, BST_CHECKED, 0);
+
+    g_hRadioCopyCodec = CreateWindow(
+        L"BUTTON", L"Copy Codec",
+        WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+        445, 515, 100, 20,
+        hwnd, (HMENU)ID_RADIO_COPY_CODEC,
+        (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), nullptr);
+    ApplyDarkTheme(g_hRadioCopyCodec);
 
     g_hRadioUseBitrate = CreateWindow(
         L"BUTTON", L"Specify Bitrate",
@@ -410,8 +410,8 @@ void RepositionControls(HWND hwnd)
     MoveWindow(g_hLabelCutInfo, audioControlsX, editingControlsY + 80, 200, 40, TRUE);
     MoveWindow(g_hButtonCut, audioControlsX, editingControlsY + 125, 200, 30, TRUE);
     MoveWindow(g_hCheckboxMergeAudio, audioControlsX, editingControlsY + 160, 200, 25, TRUE);
-    MoveWindow(g_hRadioCopyCodec, audioControlsX, editingControlsY + 190, 100, 20, TRUE);
-    MoveWindow(g_hRadioH264, audioControlsX + 105, editingControlsY + 190, 150, 20, TRUE);
+    MoveWindow(g_hRadioH264, audioControlsX, editingControlsY + 190, 120, 20, TRUE);
+    MoveWindow(g_hRadioCopyCodec, audioControlsX + 120, editingControlsY + 190, 100, 20, TRUE);
     MoveWindow(g_hRadioUseBitrate, audioControlsX, editingControlsY + 215, 100, 20, TRUE);
     MoveWindow(g_hRadioUseSize, audioControlsX + 105, editingControlsY + 215, 100, 20, TRUE);
     MoveWindow(g_hLabelBitrate, audioControlsX, editingControlsY + 240, 200, 20, TRUE);
